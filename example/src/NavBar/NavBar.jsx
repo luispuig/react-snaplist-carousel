0e079b4bd52cc7452eb609ad91f0176ed700c955
Luis Puig <mail@lpuig.com>
2020-02-29 16:58:22 +0100

    Disable next button

--- example/src/NavBar/NavBar.jsx
+++ example/src/NavBar/NavBar.jsx
@@ -6,11 +6,16 @@ import { ReactComponent as BackwardArrow } from './back-arrow.svg';
 
 import styles from './styles.module.css';
 
-const Item = ({ children, onClick }) => (
-  <div onClick={() => onClick()} className={mergeStyles(styles.link, styles.topBarItem)}>
+const Item = ({ children, onClick, disabled }) => (
+  <div
+    onClick={() => onClick()}
+    className={mergeStyles(styles.link, styles.topBarItem, disabled ? styles.topBarItem_disabled : null)}
+  >
     {children}
   </div>
 );
+
+const maxItems = 3;
 
 export const NavBar = ({ show, onSelect, selected }) => (
   <div className={mergeStyles(styles.topBar, !show ? styles.topBar_hidden : null)}>
@@ -23,8 +28,8 @@ export const NavBar = ({ show, onSelect, selected }) => (
       <Item onClick={() => onSelect(Math.max(0, selected - 1))}>
         <BackwardArrow className={styles.arrowIcon} />
       </Item>
-      <Item onClick={() => onSelect(2, Math.min(selected + 1))}>
-        <ForwardArrow className={styles.arrowIcon}/>
+      <Item onClick={() => onSelect(maxItems, Math.min(selected + 1))} disabled={selected >= maxItems}>
+        <ForwardArrow className={styles.arrowIcon} />
       </Item>
     </div>
   </div>
